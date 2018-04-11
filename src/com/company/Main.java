7105e184493829758b699c9f9b39bec0a394df1e
Nizzo <maarekn@post.bgu.ac.il>
2018-04-11 22:28:58 +0300

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//Chen, if you see this, fill in the blank:
+    //Avi Elly sucks a lot of d___.
     }
 }
